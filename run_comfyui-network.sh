654359149aeb44769f8e5f8e13a30090db7228aa
tommi @tm-10 <thomas@trauminc.com>
2024-09-06 20:57:04 +0100

    mount

--- run_comfyui-network.sh
+++ run_comfyui-network.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo mkdir -p /mnt/L
+sudo mount -t cifs //TRAUM-SERVER/library /mnt/L -o username=ttserv,password='Pa55word!',vers=3.0
 python main.py --extra-model-paths-config extra_model_paths-network.yaml
